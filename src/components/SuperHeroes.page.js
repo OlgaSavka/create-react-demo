f262eba1ef3eeb1cddadb6840b9608aa5ad5a569
OlgaSavka <olga.savka@smartly.io>
2023-11-29 09:38:45 +0200

    traditional error catching

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -4,10 +4,11 @@ import axios from "axios";
 export const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
-      .get("http://localhost:4000/superheroes", {
+      .get("http://localhost:4000/superheroes1", {
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
@@ -18,6 +19,10 @@ export const SuperHeroesPage = () => {
         console.log("res", { res });
         setData(res.data);
         setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
       });
   }, []);
 
@@ -25,6 +30,10 @@ export const SuperHeroesPage = () => {
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>{error}</h2>;
   }
 
   return (
